2a0bb6d7d623a9ae8382bfd082fec1ee4492eb8c
Maycon-Santos <mayconsantos.dev@gmail.com>
2021-04-20 22:06:52 -0300

    add cache layer

---
+++ cache/cache.go
@@ -0,0 +1,44 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type Client struct {
+	client *redis.Client
+}
+
+func NewClient(ctx context.Context, address string) (*Client, error) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: address,
+	})
+
+	_, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{client: redisClient}, nil
+}
+
+func (c Client) Get(ctx context.Context, key string) (string, error) {
+	value, err := c.client.Get(ctx, key).Result()
+	if err != nil && err != redis.Nil {
+		return "", err
+	}
+
+	return value, nil
+}
+
+func (c Client) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	status := c.client.Set(ctx, key, value, expiration)
+	err := status.Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
