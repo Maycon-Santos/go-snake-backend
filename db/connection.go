df499ad0100a8d6ec7212839c8ddf3edb9cbffd4
Maycon-Santos <mayconsantos.dev@gmail.com>
2021-04-20 22:17:21 -0300

    add db layer

---
+++ db/connection.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/Maycon-Santos/go-snake-backend/process"
+	_ "github.com/lib/pq"
+)
+
+func NewConnection(env process.Env) (*sql.DB, error) {
+	db, err := sql.Open(env.Database.Driver, env.Database.ConnURI)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
