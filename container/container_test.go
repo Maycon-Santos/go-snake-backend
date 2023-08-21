77ee520864f058bc29d443887d860777468f32f1
Maycon-Santos <mayconsantos.dev@gmail.com>
2023-08-21 19:40:17 -0300

    improve container test

--- container/container_test.go
+++ container/container_test.go
@@ -11,6 +11,25 @@ type fakeDependency2 = int
 type fakeDependency3 = bool
 type fakeDependency4 = uint8
 
+type fakeDependencyInterface1 interface {
+	Method1() interface{}
+	Method2() interface{}
+}
+
+type fakeDependencyInterface2 interface {
+	Method1() interface{}
+	Method2() interface{}
+}
+
+type fakeDependency5 struct{}
+
+func (f fakeDependency5) Method1() interface{} { return nil }
+func (f fakeDependency5) Method2() interface{} { return nil }
+
+func newFakeDependency5() fakeDependencyInterface1 {
+	return fakeDependency5{}
+}
+
 func Test_container_Inject(t *testing.T) {
 	instance := New()
 
@@ -46,9 +65,10 @@ func Test_container_Retrieve(t *testing.T) {
 	dep1 := fakeDependency1("dependency")
 	dep2 := fakeDependency2(1)
 	dep3 := fakeDependency3(true)
+	dep4 := newFakeDependency5()
 	instance := New()
 
-	instance.Inject(&dep1, &dep2, &dep3)
+	instance.Inject(&dep1, &dep2, &dep3, &dep4)
 
 	t.Run("should retrieve dependencies without errors", func(t *testing.T) {
 		var (
@@ -63,6 +83,17 @@ func Test_container_Retrieve(t *testing.T) {
 		assert.Equal(t, fakeDependency1("dependency"), abs1)
 		assert.Equal(t, fakeDependency2(1), abs2)
 		assert.Equal(t, fakeDependency3(true), abs3)
+	})
+
+	t.Run("should retrieve dependency with similar interface if don't have the original", func(t *testing.T) {
+		var (
+			abs1 fakeDependencyInterface2
+		)
+
+		err := instance.Retrieve(&abs1)
+		assert.NoError(t, err)
+
+		assert.Equal(t, fakeDependency5{}, abs1)
 	})
 
 	t.Run("should return an error when trying retrieve abstraction without initial value", func(t *testing.T) {
